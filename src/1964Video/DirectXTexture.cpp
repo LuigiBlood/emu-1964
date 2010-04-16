03addcabd1c45fdcaea4a523f425537fdbdffc60
death2droid <death2droid@888a8426-7ce0-11de-a253-d3d9d9688d98>
2010-04-16 12:35:03 +0000

    Make DX automatically generate mip maps.

--- src/1964Video/DirectXTexture.cpp
+++ src/1964Video/DirectXTexture.cpp
@@ -152,13 +152,14 @@ LPRICETEXTURE CDirectXTexture::CreateTexture(uint32 dwWidth, uint32 dwHeight, Te
 	if( m_Usage == AS_RENDER_TARGET)
 	{
 		D3DXCheckTextureRequirements(g_pD3DDev, &m_dwCreatedTextureWidth, &m_dwCreatedTextureHeight, &dwNumMaps,D3DUSAGE_AUTOGENMIPMAP | D3DUSAGE_RENDERTARGET, &pf, D3DPOOL_DEFAULT);
-		g_pD3DDev->CreateTexture(m_dwCreatedTextureWidth,m_dwCreatedTextureHeight, 0, D3DUSAGE_AUTOGENMIPMAP | D3DUSAGE_RENDERTARGET, pf, D3DPOOL_DEFAULT, &lpSurf,NULL);
+		g_pD3DDev->CreateTexture(m_dwCreatedTextureWidth,m_dwCreatedTextureHeight, 0 ,D3DUSAGE_AUTOGENMIPMAP | D3DUSAGE_RENDERTARGET, pf, D3DPOOL_DEFAULT, &lpSurf,NULL);
+		lpSurf->GenerateMipSubLevels(); // Hint to the GPU to generate the mip maps
 	}
 	else
 	{
 		D3DXCheckTextureRequirements(g_pD3DDev, &m_dwCreatedTextureWidth, &m_dwCreatedTextureHeight, &dwNumMaps, D3DUSAGE_AUTOGENMIPMAP, &pf, D3DPOOL_MANAGED);
 		hr = g_pD3DDev->CreateTexture(m_dwCreatedTextureWidth,m_dwCreatedTextureHeight, 0, D3DUSAGE_AUTOGENMIPMAP, pf, D3DPOOL_MANAGED, &lpSurf,NULL);
-		
+		lpSurf->GenerateMipSubLevels(); // Hint to the GPU to generate the mip maps
 	}
 
 #ifdef _DEBUG
