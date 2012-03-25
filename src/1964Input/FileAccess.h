4d4bc1c953816f2f02ae4d35807b7ac301c3ef5e
death2droid <death2droid@gmail.com>
2012-03-25 02:28:08 +0000

    Fix xInput settings not being loaded thanks to meejvaj Fixes issue 71

--- src/1964Input/FileAccess.h
+++ src/1964Input/FileAccess.h
@@ -152,7 +152,7 @@ unsigned long djbHash(const char *str);
 #define CHK_NOTE			2089365563
 
 #define CHK_PLUGGED			3378836077
-#define CHK_XINPUT			623987373
+#define CHK_XINPUT			3665436173
 #define CHK_RAWDATA			1238573385
 #define CHK_PAKTYPE			2936974691
 #define CHK_REALN64RANGE	1279831790
