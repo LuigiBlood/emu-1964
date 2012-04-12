b70c7b71dddf5ae3f782167922965fe6a2ba16ba
death2droid@gmail.com <death2droid@gmail.com@888a8426-7ce0-11de-a253-d3d9d9688d98>
2012-04-12 09:10:57 +0000

    Quick build fix. Fixes issue 76

--- src/core/Main.cpp
+++ src/core/Main.cpp
@@ -1,10 +1,8 @@
-#include "InterpretedOpcodes.h"
 #include <windows.h>
 
 extern int APIENTRY aWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow);
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
 {
-//	CInterpretedOpcodes* InterpretedOps = new CInterpretedOpcodes;
-	
+
 	aWinMain(hInstance, hPrevInstance, lpszCmdLine, nCmdShow);
 }
